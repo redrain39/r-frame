438fce3d9206d5617feb761761aa348ba62cdc9d
redrain <redrain39@163.com>
2022-10-25 11:30:38 +0800

    1、新增应用内文字大小控制逻辑

--- sup_base/src/main/java/com/redrain/sup_base/ui/module/impls/BaseActivity.kt
+++ sup_base/src/main/java/com/redrain/sup_base/ui/module/impls/BaseActivity.kt
@@ -1,11 +1,14 @@
 package com.redrain.sup_base.ui.module.impls
 
+import android.content.Context
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import com.redrain.sup_base.ui.module.interfaces.IBaseActivity
+import com.redrain.sup_base.utils.DisplayUtil
 import org.greenrobot.eventbus.EventBus
 
 abstract class BaseActivity<DB : ViewDataBinding>(
@@ -38,6 +41,25 @@ abstract class BaseActivity<DB : ViewDataBinding>(
         if (useEventBus()) {
             EventBus.getDefault().unregister(this)
         }
+    }
+
+    private var fontScale = 1f
+
+    override fun getResources(): Resources {
+        val resources = super.getResources()
+        return DisplayUtil.getResources(this, resources, fontScale)
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(DisplayUtil.attachBaseContext(newBase!!, fontScale))
+    }
+
+    /**
+     * 动态设置应用内文字大小
+     */
+    fun setFontScale(fontScale: Float) {
+        this.fontScale = fontScale
+        DisplayUtil.recreate(this)
     }
 
     open fun initStatusBar() {
