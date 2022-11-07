44794aa96454c0e70d5f01b71edd3978ee95d6ab
redrain <redrain39@163.com>
2022-11-07 16:42:34 +0800

    1、ViewModel基础封装 2、修改库版本为1.0.8

---
+++ sup_base/src/main/java/com/redrain/sup_base/manager/LogTagManager.kt
@@ -0,0 +1,6 @@
+package com.redrain.sup_base.manager
+
+object LogTagManager {
+
+    const val TAG_NETWORK = "网络请求"
+}
--- sup_base/src/main/java/com/redrain/sup_base/ui/viewmodel/impls/BaseViewModel.kt
+++ sup_base/src/main/java/com/redrain/sup_base/ui/viewmodel/impls/BaseViewModel.kt
@@ -1,8 +1,34 @@
 package com.redrain.sup_base.ui.viewmodel.impls
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.redrain.sup_base.manager.LogTagManager
 import com.redrain.sup_base.ui.viewmodel.interfaces.IBaseViewModel
+import com.redrain.sup_base.utils.LogUtil
+import com.redrain.sup_base.utils.ToastUtil
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 abstract class BaseViewModel: ViewModel(), IBaseViewModel {
+
+    fun launch(
+        onLaunch: suspend CoroutineScope.() -> Unit,
+        onError: suspend CoroutineScope.(e: Exception) -> Unit = {},
+        onComplete: suspend CoroutineScope.() -> Unit = {}
+    ) {
+        viewModelScope.launch {
+            try {
+                onLaunch()
+            } catch (e: Exception) {
+                onError(e)
+                ToastUtil.showShortToast("网络请求错误，请稍后再试")
+                LogUtil.e(LogTagManager.TAG_NETWORK, e.toString())
+            } finally {
+                onComplete()
+            }
+        }.invokeOnCompletion { t ->
+            if (t != null) LogUtil.e(LogTagManager.TAG_NETWORK, t.toString())
+        }
+    }
 
 }
